2da01f66636f7a0cc6408cfd6894a998c3a37b7b
Litanu <arichardcassady@gmail.com>
2026-06-27 20:11:07 -0400

    Quartz sync: Jun 27, 2026, 8:11 PM

--- quartz.ts
+++ quartz.ts
@@ -1,13 +1,6 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 import * as ExternalPlugin from "./.quartz/plugins"
 
-ExternalPlugin.Explorer({
-  mapFn: (node) => {
-    node.displayName = node.displayName.replaceAll("-", " ")
-    return node
-  },
-})
-
 const config = await loadQuartzConfig()
 export default config
 export const layout = await loadQuartzLayout()
